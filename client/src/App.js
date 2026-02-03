ad2e4c1e8c16b9d7a8c76f1a552158afcde3dbde
DH540 <dylanaguillosalazar@gmail.com>
2026-02-03 22:32:33 +0800

    Commit inputSection.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import Header from './components/header';
 import Hero from './components/hero';
+import InputSection from './components/inputSection';
 import './App.css';
 
 function App() {
+  const [textToReview, setTextToReview] = React.useState("");
+  const [selectedModel, setSelectedModel] = React.useState("");
+  
+  const handleAnalyze = () => {
+    console.log("Text: ", textToReview);
+    console.log("Model: ", selectedModel);
+  }
+
   return (
     <div className="App">
       <Header />
       <Hero />
+      <InputSection 
+        textToReview={textToReview}
+        onChange={setTextToReview}
+        selectedModel={selectedModel}
+        onModelChange={setSelectedModel}
+        onAnalyze={handleAnalyze}
+      />
       {/* Other components can be added here */}
     </div>
   );
